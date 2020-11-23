7bd5db6a5f7ac05685e8dd4ec84b9db02c3ec000
preetitanishka <71656803+preetitanishka@users.noreply.github.com>
2020-11-23 18:54:28 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+//Create variables here
+var dog, happydog,database,foodS,foodStock
+function preload()
+{
+  //load images here
+  dog=loadImage("doglmg.png");
+  happydog=loadImage("doglmg1.png");
+}
+
+function setup() {
+  createCanvas(500, 500);
+  dog = createSprite(200,200,20,20);
+  dog.addImage(doglmg.png);
+  dog.scaleImage = 0.2;
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
